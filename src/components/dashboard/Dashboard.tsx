55eef2d9a8f50574e17d3567175259f085346e4a
rohit-2 <rohit4444choudhary@gmail.com>
2023-09-21 13:53:44 +0530

    snakcbar added

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -2,9 +2,7 @@ import { useState } from 'react';
 
 export const Dashboard = () => {
   const [value, setValue] = useState(0);
-
-  const sendRequest = () => {
-  };
+  
 
   return <div>
     <button onClick={() => {
