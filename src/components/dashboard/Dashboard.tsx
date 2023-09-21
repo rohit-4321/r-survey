3e35ba4e3fa604296321487fff05ad21b476df4f
rohit-2 <rohit4444choudhary@gmail.com>
2023-09-21 13:49:30 +0530

    with sackbar

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,29 +1,9 @@
 import { useState } from 'react';
-import axiosInstance from '../../api/axios';
 
 export const Dashboard = () => {
   const [value, setValue] = useState(0);
-  // const user = useUserContext();
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const sendRequest = () => {
-    firebaseAuth.currentUser?.getIdToken(true)
-      .then((tk) => {
-        axiosInstance({
-          method: 'get',
-          url: '/loguser',
-          headers: {
-            'Authorization': `Bearer ${tk}`
-          }
-        }).then(() => {
-          console.log('SUccessfull');
-        }).catch(() => {
-          console.log('failed');
-        });
-      })
-      .catch(() => {
-        console.log('Error on getting token');
-      });
   };
 
   return <div>
