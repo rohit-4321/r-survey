e6cc1a6d87acfcbd89dd049d04ec8cc2d9a1aa16
rohit-2 <rohit4444choudhary@gmail.com>
2023-10-03 16:53:26 +0530

    modify loading icon and initial join screen

--- src/assets/LoadingSVG.tsx
+++ src/assets/LoadingSVG.tsx
@@ -1,8 +1,9 @@
 type Props = React.ComponentProps<'svg'>
 export const LoadingSVG = (props: Props) => {
-  return <svg {...props} version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-    viewBox="0 0 100 100" enableBackground="new 0 0 0 0" xmlSpace="preserve">
-    <path fill="#fff" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
+  return <svg {...props} fill="#ffffff" viewBox="20 20 60 60" enableBackground="new 0 0 0 0" xmlSpace="preserve">
+    <path 
+      d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"
+    >
       <animateTransform 
         attributeName="transform" 
         attributeType="XML" 
@@ -10,7 +11,8 @@ export const LoadingSVG = (props: Props) => {
         dur="1s" 
         from="0 50 50"
         to="360 50 50" 
-        repeatCount="indefinite" />
+        repeatCount="indefinite" 
+      />
     </path>
   </svg>;
 };
